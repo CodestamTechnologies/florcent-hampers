7d9aabbedb846d788cf47a87a3fe467765df07bd
Hardish singh <hardishrajpu35@gmail.com>
2025-04-19 15:29:31 +0530

    added search funtionality with Command line

--- src/components/main/header.tsx
+++ src/components/main/header.tsx
@@ -6,6 +6,8 @@ import { Bell, Menu, Search, ShoppingCart, User } from 'lucide-react'
 import { Input } from '../ui/input'
 import { useCart } from '@/providers/cartProvider'
 import { useAuth } from '@/providers/authProvider'
+import SearchProducts from './Search'
+import { products } from '@/data'
 
 const Header = () => {
     const [notificationCount] = useState(2);
@@ -21,13 +23,7 @@ const Header = () => {
                     <div className="text-2xl font-serif font-medium">Élégance</div>
                 </div>
                 <div className="hidden md:flex items-center space-x-4">
-                    <div className="relative">
-                        <Input
-                            placeholder="Search for products..."
-                            className="w-64 pl-8 rounded-full bg-gray-50 border-gray-200 focus:bg-white"
-                        />
-                        <Search className="h-4 w-4 absolute left-2.5 top-2.5 text-gray-500" />
-                    </div>
+                    <SearchProducts products={products}/>
                     <Button variant="ghost" size="icon" className="relative">
                         <Bell className="h-5 w-5" />
                         {notificationCount > 0 && (
